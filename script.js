f449a9fb4a70b4e50f4f6926a8afbe49b864d16a
Raphael Kurochkin <raphaeloper@gmail.com>
2021-04-08 18:59:55 +0300

    Add players and game object

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 import { cardTypes, cardColors, cardIDs, cards } from "./misc/cards.js";
 
+const game = {};
+
 function createDeck() {
     let deck = [];
     cards.forEach((card) => {
@@ -31,9 +33,60 @@ function randomInt(max) {
     return Math.floor(Math.random() * max);
 }
 
-function shuffleDeck(deck) {
+function shuffle(deck) {
     for (let cardIndex = deck.length - 1; cardIndex > 0; cardIndex--) {
         let secondIndex = randomInt(cardIndex + 1);
         [deck[cardIndex], deck[secondIndex]] = [deck[secondIndex], deck[cardIndex]]
     }
 }
+
+function createPlayer(playerName) {
+    const player = {
+        name: playerName,
+        hand: []
+    }
+
+    return player;
+}
+
+function createPlayersFromNameList(...playerNames) {
+    const players = [];
+    [...playerNames].forEach((playerName) => {
+        const player = createPlayer(playerName);
+        players.push(player);
+    });
+
+    return players;
+}
+
+function dealCardsToPlayer(player, numOfCards) {
+    for(let cardsDealt = 0; cardsDealt < numOfCards; cardsDealt++){
+        if(isDeckEmpty()){
+            reshufflePile(game.pile, game.deck)
+        }
+
+        player.hand.push(game.deck.pop());
+    }
+}
+
+function reshufflePile(pile, deck){
+    const topCard = pile.pop();
+    shuffle(pile);
+    deck = pile;
+    pile = topCard;
+}
+
+function isDeckEmpty() {
+    return game.deck.length === 0;
+}
+
+game.players = createPlayersFromNameList("Me", "Them", "You");
+game.deck = createDeck();
+game.pile = [];
+game.currentPlayer = game.players[randomInt(game.players.length)];
+shuffle(game.deck);
+game.players.forEach((player) => {
+    dealCardsToPlayer(player, 8);
+});
+
+console.dir(game);
